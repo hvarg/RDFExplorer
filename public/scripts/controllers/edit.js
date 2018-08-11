89219ed22a4bc75c9a6b2022e99edcdb9ae2a117
Hernán Vargas <hernan.vargas@alumnos.usm.cl>
2018-08-10 21:42:19 -0400

    edit form WIP

--- public/scripts/controllers/edit.js
+++ public/scripts/controllers/edit.js
@@ -1,10 +1,16 @@
 angular.module('rdfvis.controllers').controller('EditCtrl', EditCtrl);
 
-EditCtrl.$inject = ['$scope', 'propertyGraphService'];
+EditCtrl.$inject = ['$scope', 'propertyGraphService', 'requestService', 'queryService'];
 
-function EditCtrl ($scope, pGraph) {
+function EditCtrl ($scope, pGraph, request, query) {
   var vm = this;
   vm.selected = null;
+  vm.label = null;
+  vm.class = null;
+  vm.loading = false;
+  vm.noResults = false;
+
+  vm.getClasses = getClasses;
 
   pGraph.edit = edit;
 
@@ -13,6 +19,11 @@ function EditCtrl ($scope, pGraph) {
 
     $scope.$emit('tool', 'edit');
     $scope.$emit('setSelected', obj);
+  }
+
+  function getClasses (label) {
+    var q = query.search(label, 'http://www.w3.org/2002/07/owl#Class');
+    return request.execQuery(q, data => { return data.results.bindings; });
   }
 
 }
